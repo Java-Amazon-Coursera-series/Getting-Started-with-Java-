4d5f019632f223df036a107398b9b97d6348224d
hammad-ibrahim <hammad.ibrahim21994@gmail.com>
2024-11-20 05:50:07 +0200

    Java01_ module 01 _ La01>> Hello world

---
+++ src/Simple_app_withJava.java
@@ -0,0 +1,6 @@
+public class Simple_app_WIthJava {
+    public static void main(String []args){
+        // printing out the line Hello world form Java
+        System.out.println("Hello world");
+    }
+}
