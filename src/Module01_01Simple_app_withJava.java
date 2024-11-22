82bd6e45b607525d806be0c440a1355f0760ceff
hammad-ibrahim <hammad.ibrahim21994@gmail.com>
2024-11-22 03:32:16 +0200

    updating file one and declaring file tow with a simple app in java

--- src/Module01_01Simple_app_withJava.java
+++ src/Module01_01Simple_app_withJava.java
@@ -1,4 +1,4 @@
-public class Simple_app_WIthJava {
+public class Module01_01Simple_app_withJava {
     public static void main(String []args){
         // printing out the line Hello world form Java
         System.out.println("Hello world");
---
+++ src/Module01_02Activity_Declaring_Vars.java
@@ -0,0 +1,20 @@
+public class Module01_02Activity_Declaring_Vars {
+    //Activity: Practice declaring and initializing variables
+    public static void main (String[]args){
+            String make = "Isuzu";
+            String model= "Super Faster KB ";
+            int year = 2001;
+            double mileAge =700000;
+            boolean isAutomatic = false;
+
+            System.out.println("Car make :" + make);
+            System.out.println("Care Model :  "+ model);
+            System.out.println("Car Year : "+ year);
+            System.out.println("Car Mileage :  "+mileAge);
+            System.out.println("Is the care automatic ?  "+ isAutomatic);
+
+    }
+
+}
+
+
